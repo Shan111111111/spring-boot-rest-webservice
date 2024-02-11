be7f9fc7c8dcdcebba6159f878e0bacef56da851
Shantesh <shanthu1804@gmail.com>
2024-02-11 13:13:15 +0530

    spring-boot-rest-webservice with spring 3.2.2 built on java 17 and also our this project too build and deployed and tested by running in local with java 17 jdk. THIS is to just start with template :p

---
+++ src/main/java/com/shantesh/springbootrestwebservice/SpringBootRestWebserviceApplication.java
@@ -0,0 +1,14 @@
+package com.shantesh.springbootrestwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestWebserviceApplication.class, args);
+		System.out.println("developing rest/json format data output producing api.....");
+	}
+
+}
---
+++ src/test/java/com/shantesh/springbootrestwebservice/SpringBootRestWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shantesh.springbootrestwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
