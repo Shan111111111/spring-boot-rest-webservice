f9d47a15637cc7f0cea36366143639072c20b092
Shantesh <shanthu1804@gmail.com>
2024-02-11 14:31:11 +0530

    added another HelloWorld Bean/Object with value response sending method to controller, just to test how object can be returned as JSON format from api as resonse, and json response data is verified in client Chrome

--- src/main/java/com/shantesh/springbootrestwebservice/controller/HelloWorldController.java
+++ src/main/java/com/shantesh/springbootrestwebservice/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.shantesh.springbootrestwebservice.controller;
 
+import com.shantesh.springbootrestwebservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,13 @@ public class HelloWorldController {
     public String sayHello(){
         return "Hi Welcome to Hello world .....";
     }
+
+    @GetMapping("/hello-bean")
+    @ResponseBody
+    public HelloWorldBean sayHelloByReturningBeanYaObject(){
+        return new HelloWorldBean("Hi Welcome to Hello world .....");
+    }
+
 
 
 }
