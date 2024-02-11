355b41101d247c78020c23198c874f06e84cdc14
Shantesh <shanthu1804@gmail.com>
2024-02-11 17:03:40 +0530

    request hit with pathvariable value in it, and code is written to capture the pathvariable and then it is sent back by populating it's value in Object(HelloWorld here dummy understanding representation)

--- src/main/java/com/shantesh/springbootrestwebservice/controller/HelloWorldController.java
+++ src/main/java/com/shantesh/springbootrestwebservice/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.shantesh.springbootrestwebservice.controller;
 
 import com.shantesh.springbootrestwebservice.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/")
-    @ResponseBody
     public String sayHello(){
         return "Hi Welcome to Hello world .....";
     }
 
     @GetMapping("/hello-bean")
-    @ResponseBody
     public HelloWorldBean sayHelloByReturningBeanYaObject(){
         return new HelloWorldBean("Hi Welcome to Hello world .....");
+    }
+
+    @GetMapping("/hello-bean/path-variable/{name}")
+    public HelloWorldBean sayHelloByReturningBeanYaObject(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hi man welcome, you came by path variable --> %s", name));
     }
 
 
