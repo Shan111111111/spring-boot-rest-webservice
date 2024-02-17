5f1195b748ddd18125adca2ba5192517ee52adbe
Shantesh <shanthu1804@gmail.com>
2024-02-18 01:25:24 +0530

    Dynamic Filtering of fields are added and one method call will return 2 fields and other method call will return other 2 fields mentioned and this is dynamic filtering behavior is achieved and tested in Postman

--- src/main/java/com/shantesh/springbootrestwebservice/filtering/FilteringController.java
+++ src/main/java/com/shantesh/springbootrestwebservice/filtering/FilteringController.java
@@ -1,5 +1,9 @@
 package com.shantesh.springbootrestwebservice.filtering;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +14,24 @@ import java.util.List;
 public class FilteringController {
 
     @GetMapping("/filtering")
-    public SomeBean retrieveSomeBean(){
-        return new SomeBean("value1", "value2", "value3");
+    public MappingJacksonValue retrieveSomeBean(){
+        SomeBean  someBean = new SomeBean("value1", "value2", "value3");
+
+        return getMappingJacksonValue("field2", "field3", new MappingJacksonValue(someBean));
+    }
+
+    private static MappingJacksonValue getMappingJacksonValue(String fieldA, String fieldB, MappingJacksonValue someBean) {
+        SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter.filterOutAllExcept(fieldA, fieldB);
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", simpleBeanPropertyFilter);
+        MappingJacksonValue mappingJacksonValue = someBean;
+        mappingJacksonValue.setFilters(filters);
+        return mappingJacksonValue;
     }
 
     @GetMapping("/filtering-for-list-of-objects")
-    public List<SomeBean> retrieveListSomeBean(){
-        return Arrays.asList( new SomeBean("value1", "value2", "value3"), new SomeBean("value11", "value22", "value33"));
+    public MappingJacksonValue retrieveListSomeBean(){
+        List list = Arrays.asList( new SomeBean("value1", "value2", "value3"), new SomeBean("value11", "value22", "value33"));
+        return getMappingJacksonValue("field1", "field2", new MappingJacksonValue(list));
     }
 }
