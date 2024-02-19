a2429a54b580a4d7e63d0f0d80a39ac940a19499
Shantesh <shanthu1804@gmail.com>
2024-02-20 00:02:03 +0530

    data.sql is created with some insert queries and data is present in db and tested in h2 console

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1, sysdate(), 'AB');
+insert into user values(2, sysdate(), 'ABg');
+insert into user values(3, sysdate(), 'ABee');
+insert into user values(345, sysdate(), 'dddAB');
