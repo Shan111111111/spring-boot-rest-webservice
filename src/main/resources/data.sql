7fe2c20127b2bc629d6b5d036fb990537f33e0d5
Shantesh <shanthu1804@gmail.com>
2024-02-20 09:25:23 +0530

    Inserted some post values into post table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 insert into user values(10001, sysdate(), 'AB');
 insert into user values(10002, sysdate(), 'ABg');
 insert into user values(10003, sysdate(), 'ABee');
-insert into user values(10005, sysdate(), 'dddAB');
+insert into user values(10005, sysdate(), 'ABee');
+insert into post values(1105, 'My first post', 10001);
+insert into post values(1106, 'My second post', 10001);
